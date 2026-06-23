66bbcd99acb90cc15f6011466eecdfaf45300cf3
Mohammed Zoheir Abbes <mohammedzoheirabbes@gmail.com>
2026-06-23 22:49:47 +0200

    Add Math POW function

--- FRONTEND WEB DEVELOPMENT/JAVASCRIPT WORK/bmi calculator/practice10.js
+++ FRONTEND WEB DEVELOPMENT/JAVASCRIPT WORK/bmi calculator/practice10.js
@@ -1,8 +1,8 @@
 // BMI (body mass index) is a measure of body fat based on height and weight that applies to adult men and women.
 
 function bodyMassIndex(height, weight) {
-  return weight / height ** 2;
+  return weight / Math.pow(height, 2);
 }
 
 var bmi = bodyMassIndex(1.8, 65);
-console.log(Math.floor(bmi));
+console.log(Math.round(bmi));
