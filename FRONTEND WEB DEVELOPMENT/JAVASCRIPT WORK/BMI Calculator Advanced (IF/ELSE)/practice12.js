2405f2744756b4a48d852bbc8f5073b7775c5a16
Mohammed Zoheir Abbes <mohammedzoheirabbes@gmail.com>
2026-06-25 17:40:56 +0200

    Building a BMI calculator advance using (IF/ELSE) in js

---
+++ FRONTEND WEB DEVELOPMENT/JAVASCRIPT WORK/BMI Calculator Advanced (IF/ELSE)/practice12.js
@@ -0,0 +1,13 @@
+function bmiCalculatorAdvance(weight, height) {
+  var BMI = weight / Math.pow(height, 2);
+  var interpretation;
+  if (BMI < 18.5) {
+    interpretation = "Your BMI is " + BMI + ", so you are underweight.";
+  } else if (BMI >= 18.5 && BMI <= 24.9) {
+    interpretation = "Your BMI is " + BMI + ", so you have a normal weight.";
+  } else {
+    interpretation = "Your BMI is " + BMI + ", so you are overweight.";
+  }
+  return interpretation;
+}
+console.log(bmiCalculatorAdvance(65, 1.8));
