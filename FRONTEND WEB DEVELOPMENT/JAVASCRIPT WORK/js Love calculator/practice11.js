30020df5d4e763451c6fff9bf6375e81b8e3a450
Mohammed Zoheir Abbes <mohammedzoheirabbes@gmail.com>
2026-06-25 16:45:37 +0200

    Creating a love calculator in JS

---
+++ FRONTEND WEB DEVELOPMENT/JAVASCRIPT WORK/js Love calculator/practice11.js
@@ -0,0 +1,12 @@
+prompt("enter your name:");
+prompt("enter your crush name");
+
+var loveCalculator = Math.random();
+loveCalculator = loveCalculator * 100;
+loveCalculator = Math.floor(loveCalculator) + 1;
+
+if (loveCalculator === 100) {
+  alert(loveCalculator + "%", " WOW");
+} else {
+  alert(loveCalculator + "%");
+}
